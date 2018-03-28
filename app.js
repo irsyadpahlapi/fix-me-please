d805611835837540e975f18cceca1936004d5aad
irsyadpahlapi <irsyadpahlapi28@gmail.com>
2018-03-28 10:12:52 -0400

    done err app without controller

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const app = express;
+var express = require('express');
+const app = express();
 
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
-var books = require('routes/books');
-var transactions = require('routes/transactions');
+
+var books = require('./routes/books');
+var transactions = require('./routes/transactions');
 
 app.use('/books', books);
 app.use('/transactions', transactions);
 
 app.listen(3000)
+
+module.exports = app;
